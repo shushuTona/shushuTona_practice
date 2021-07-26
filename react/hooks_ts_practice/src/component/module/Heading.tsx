1ccf3ca33a9eaa2d77a8c92db51aaaa6c6d02451
Lettuce0605 <nagase.syunnsuke@gmail.com>
2021-07-27 00:01:01 +0900

    見出しコンポーネント作成

---
+++ react/hooks_ts_practice/src/component/module/Heading.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './css/Heading.css';
+
+type headingGroup = ( 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' );
+interface Props  {
+    text: string,
+    htmlHeadingTag: keyof JSX.IntrinsicElements & headingGroup
+}
+
+const Heading = React.memo( ( { text, htmlHeadingTag }: Props) => {
+    console.log( 'Heading' );
+
+    const CustomTag = htmlHeadingTag;
+
+    return (
+        <CustomTag className="m-heading"><span>{ text }</span></CustomTag>
+    );
+} );
+
+export { Heading };
