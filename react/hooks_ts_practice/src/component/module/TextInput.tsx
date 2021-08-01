5d6f8f191c1ef4913b14b4ddadc36ffdc2e174ee
Lettuce0605 <nagase.syunnsuke@gmail.com>
2021-08-01 20:02:19 +0900

    入力コンポーネントにプレースホルダ追加

--- react/hooks_ts_practice/src/component/module/TextInput.tsx
+++ react/hooks_ts_practice/src/component/module/TextInput.tsx
@@ -5,18 +5,28 @@ interface Props {
     inputType: 'input' | 'textarea',
     inputValue: string,
     labelText: string,
+    placeholder: string,
     changeInputHandler: ChangeEventHandler
 }
 
-const TextInput = memo( ( { inputType, inputValue, labelText, changeInputHandler }: Props ) => {
+const TextInput = memo( ( { inputType, inputValue, labelText, placeholder, changeInputHandler }: Props ) => {
     console.log('TextInput');
 
     return (
         <label className="m-textInput">
             {
                 inputType === 'input' ?
-                    <input className="textInput__input" value={inputValue} onChange={changeInputHandler} type="text" /> :
-                    <textarea className="textInput__input" value={inputValue} onChange={changeInputHandler} />
+                    <input
+                        className="textInput__input"
+                        type="text"
+                        value={inputValue}
+                        onChange={changeInputHandler}
+                        placeholder={placeholder} /> :
+                    <textarea
+                        className="textInput__input"
+                        value={inputValue}
+                        onChange={changeInputHandler}
+                        placeholder={placeholder} />
             }
             <span className="textInput__label">{labelText}</span>
         </label>
