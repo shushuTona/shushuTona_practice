0ce8320dcf5a6b3bf0582188d950263434193d5c
Lettuce0605 <nagase.syunnsuke@gmail.com>
2021-08-01 16:57:33 +0900

    アイテムパネルコンポーネント作成

---
+++ react/hooks_ts_practice/src/component/module/Panel.tsx
@@ -0,0 +1,111 @@
+import React, { ChangeEventHandler, MouseEventHandler, useEffect, useCallback, useRef } from 'react';
+import './css/Panel.css';
+
+interface Props {
+    panelID: string,
+    panelTitle: string,
+    panelDesc: string,
+    panelStatus: 'Standby' | 'Running' | 'Finish'
+    changePanelHandler: ChangeEventHandler
+}
+
+const Panel = React.memo( ( { panelID, panelTitle, panelDesc, panelStatus, changePanelHandler }: Props ) => {
+    console.log( 'Panel' );
+
+    const rootRef = useRef<HTMLDivElement>(null);
+    const panelRef = useRef<HTMLDivElement>(null);
+    const panelInnerRef = useRef<HTMLDivElement>( null );
+
+    const clickHandler: MouseEventHandler<HTMLButtonElement> = useCallback( () => {
+        if (
+            rootRef.current &&
+            panelRef.current &&
+            panelInnerRef.current
+        ) {
+            if ( rootRef.current.classList.contains( 'is-open' ) ) {
+                panelRef.current.style.height = `${panelInnerRef.current.clientHeight}px`;
+
+                setTimeout( () => {
+                    if (
+                        rootRef.current &&
+                        panelRef.current &&
+                        panelInnerRef.current
+                    ) {
+                        panelRef.current.style.height = `0`;
+                    }
+                }, 50 );
+            } else {
+                panelRef.current.style.height = `0`;
+
+                setTimeout( () => {
+                    if (
+                        rootRef.current &&
+                        panelRef.current &&
+                        panelInnerRef.current
+                    ) {
+                        panelRef.current.style.height = `${panelInnerRef.current.clientHeight}px`;
+                        rootRef.current.classList.add( 'is-open' );
+                    }
+                }, 50);
+            }
+        }
+    }, [] );
+
+    // 初回表示用のアニメーション後に該当classを削除する
+    useEffect( () => {
+        if (
+            rootRef.current &&
+            panelRef.current &&
+            panelInnerRef.current
+        ) {
+            const animationendHandler = ( event: Event ) => {
+                rootRef.current?.classList.remove( 'is-show' );
+
+                rootRef.current?.removeEventListener( 'animationend', animationendHandler );
+            }
+
+            rootRef.current.addEventListener( 'animationend', animationendHandler );
+
+            panelRef.current.addEventListener( 'transitionend', ( event ) => {
+                if (
+                    rootRef.current &&
+                    panelRef.current &&
+                    panelInnerRef.current
+                ) {
+                    console.log( panelRef.current.style.height );
+
+                    if ( panelRef.current.clientHeight === 0 ) {
+                        console.log('閉じた後');
+
+                        rootRef.current.classList.remove( 'is-open' );
+                    } else if ( rootRef.current.classList.contains( 'is-open' )) {
+                        console.log('開けた後');
+
+                        panelRef.current.style.height = 'auto';
+                    }
+                }
+            } );
+        }
+    }, [] );
+
+    return (
+        <div className="m-panel is-show" ref={rootRef}>
+            <div className="panel__inner">
+                <div className="panel__title">
+                    <input id={panelID} className="panel__input" type="checkbox" onChange={changePanelHandler} />
+                    <label className="panel__inputLabel" htmlFor={panelID}></label>
+                    <button className="panel__btn" type="button" onClick={clickHandler}>
+                        <span className="panel__status">{panelStatus}</span>
+                        <span>{panelTitle}</span>
+                    </button>
+                </div>
+
+                <div className="panel__desc" ref={panelRef}>
+                    <div className="panel__descInner" ref={panelInnerRef}>{panelDesc}</div>
+                </div>
+            </div>
+        </div>
+    );
+} );
+
+export { Panel };
