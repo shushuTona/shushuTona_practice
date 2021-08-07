bc0b3e9bc794d66b4bac8f48fb7e8765c8d5d36b
Lettuce0605 <nagase.syunnsuke@gmail.com>
2021-08-07 20:33:55 +0900

    細かい調整

--- react/hooks_ts_practice/src/component/layout/Header.tsx
+++ react/hooks_ts_practice/src/component/layout/Header.tsx
@@ -9,7 +9,6 @@ const Header = React.memo( () => {
         <header className="l-header">
             <div className="header__inner">
                 <Link className="header__logo" to="/">LOGO</Link>
-                <div className="header__account">ログインした名前</div>
             </div>
         </header>
     );
