70cd8c6fe2c5c53d68f684a101ecccd2cf26c93c
Lettuce0605 <nagase.syunnsuke@gmail.com>
2021-11-24 17:28:24 +0900

    next_practice：tutorial

--- nextjs/nextjs-blog/src/pages/index.js
+++ nextjs/nextjs-blog/src/pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import { getSortedPostsData } from '../../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../../lib/posts'
 
 export default function Home ( { allPostsData }) {
   return (
@@ -11,7 +13,7 @@ export default function Home ( { allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>Hello! It's dummy site.</p>
+        <p>Hello! It&apos;s dummy site.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -23,11 +25,13 @@ export default function Home ( { allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map( ( { id, date, title } ) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${ id }`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ) )}
         </ul>
